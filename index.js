945cd66f39c982c266982b4e2506a6b0c5a73154
Hugo Alliaume <kocal@live.fr>
2018-06-14 22:47:06 +0200

    chore: remove useless files

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const path = require('path')
 const compiler = require('vue-template-compiler')
 
 require('./lib/requireHookForVue')
 const vueTag = require('./lib/vueTag')
-const docletHandlers = require('./lib/docletHandlers')
 
 exports.handlers = {
   beforeParse (e) {
@@ -12,30 +10,12 @@ exports.handlers = {
 
       e.source = parsedComponent.script ? parsedComponent.script.content : ''
     }
-  },
-  newDoclet (e) {
-    if (e.doclet.scope === 'vue') {
-      const file = path.join(e.doclet.meta.path, e.doclet.meta.filename)
-      const vueComponentPrototype = null //TODO: to be removed
-
-      /*
-       * Dirty tricks, only supports default template at the moment.
-       * We should find a way to write subsections like « Methods » one,
-       * outside this doclet description...
-       */
-      e.doclet.description = `</p></div></div>`
-
-      docletHandlers.handleProps(e, vueComponentPrototype)
-      docletHandlers.handleComputed(e, vueComponentPrototype)
-      docletHandlers.handleData(e, vueComponentPrototype)
-
-      e.doclet.description += `<div class="container-overview"><div><p>`
-    }
-
-    docletHandlers.handleMethods(e)
   }
 }
 
 exports.defineTags = function (dictionary) {
   dictionary.defineTag(vueTag.name, vueTag.options)
+  dictionary.defineTag(vueDataTag.name, vueDataTag.options)
+  dictionary.defineTag(vuePropTag.name, vuePropTag.options)
+  dictionary.defineTag(vueComputedTag.name, vueComputedTag.options)
 }
