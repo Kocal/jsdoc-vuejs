89e2d8dc43affe42111dc83d468bb9e32bd6db32
Hugo Alliaume <kocal@live.fr>
2018-06-15 07:46:22 +0200

    chore: work on default renderer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const compiler = require('vue-template-compiler')
 
 require('./lib/requireHookForVue')
+const defaultRenderer = require('./lib/renderers/default');
 const vueDataTag = require('./lib/tags/vue-data')
 const vuePropTag = require('./lib/tags/vue-prop')
 const vueComputedTag = require('./lib/tags/vue-computed')
@@ -11,6 +12,29 @@ exports.handlers = {
       const parsedComponent = compiler.parseComponent(e.source)
 
       e.source = parsedComponent.script ? parsedComponent.script.content : ''
+    }
+  },
+  newDoclet (e) {
+    if (e.doclet.meta.filename.endsWith('.vue')) {
+      const componentName = e.doclet.meta.filename.replace(/\.vue$/, '')
+
+      // if (e.doclet.memberof === 'module.exports') {
+      //   e.doclet.memberof = componentName
+      // }
+      //
+      // if (e.doclet.longname.startsWith('module.exports.')) {
+      //   e.doclet.longname = e.doclet.longname.replace('module.exports.', componentName)
+      // }
+
+      if (e.doclet._isVueDoc) {
+        const props = e.doclet._vueProps || []
+        const data = e.doclet._vueData || []
+        const computed = e.doclet._vueComputed || []
+
+        e.doclet.kind = 'module'
+        e.doclet.name = e.doclet.alias = e.doclet.longname = componentName
+        e.doclet.description = defaultRenderer(e.doclet.description || '', props, data, computed)
+      }
     }
   }
 }
