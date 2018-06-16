5c6e7180b63df3d8a4b93bcd3f8881125411ca27
Hugo Alliaume <kocal@live.fr>
2018-06-16 22:02:41 +0200

    chore(test): remove tests for renderers, because Cypress will test them

--- __tests__/core/__fixtures__/computed.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-  {
-    name: 'message',
-    type: {
-      names: ['String']
-    },
-    description: 'A message'
-  }
-]
--- __tests__/core/__fixtures__/data.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-  {
-    name: 'counter',
-    type: {
-      names: ['Number']
-    },
-    description: "Current counter's value"
-  }
-]
