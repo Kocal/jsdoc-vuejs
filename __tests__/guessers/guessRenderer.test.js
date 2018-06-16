6dda43f0a2d5c5f17653b9d715cf1819d4744b4f
Hugo Alliaume <kocal@live.fr>
2018-06-16 07:55:08 +0200

    feat: add guessRenderer

---
+++ __tests__/guessers/guessRenderer.test.js
@@ -0,0 +1,31 @@
+const guessRenderer = require('../../lib/guessers/guessRenderer')
+
+describe('guessRenderer', () => {
+  test('guess default', () => {
+    expect(guessRenderer()).toBe('default')
+    expect(guessRenderer('default')).toBe('default')
+  })
+
+  test('guess docstrap', () => {
+    expect(guessRenderer('./node_modules/ink-docstrap/template')).toBe('docstrap')
+  })
+
+  test('guess minami', () => {
+    expect(guessRenderer('node_modules/minami')).toBe('minami')
+  })
+
+  test('guess pixi', () => {
+    expect(guessRenderer('@pixi/jsdoc-template')).toBe('pixi')
+  })
+
+  test('guess tui', () => {
+    expect(guessRenderer('node_modules/tui-jsdoc-template')).toBe('tui')
+  })
+
+  test('guess unsupported', () => {
+    console.warn = jest.fn()
+
+    expect(guessRenderer('foo-bar')).toBe('default')
+    expect(console.warn).toHaveBeenCalledWith(`The template "foo-bar" is not recognized by jsdoc-vuejs. Using default template as fallback.`)
+  })
+})
