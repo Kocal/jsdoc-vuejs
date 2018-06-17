b9864c64aa368c6df43aa05b8d78d666c752e0b0
Hugo Alliaume <kocal@live.fr>
2018-06-17 09:11:21 +0200

    chore: replace ESLint Standard with AirBnb (#62)

--- __tests__/guessers/guessRenderer.test.js
+++ __tests__/guessers/guessRenderer.test.js
@@ -1,27 +1,29 @@
-const guessRenderer = require('../../lib/core/guessRenderer')
+/* eslint-disable no-console */
+
+const guessRenderer = require('../../lib/core/guessRenderer');
 
 describe('guessRenderer', () => {
   test('guess default', () => {
-    expect(guessRenderer()).toBe('default')
-    expect(guessRenderer('default')).toBe('default')
-  })
+    expect(guessRenderer()).toBe('default');
+    expect(guessRenderer('default')).toBe('default');
+  });
 
   test('guess docstrap', () => {
-    expect(guessRenderer('./node_modules/ink-docstrap/template')).toBe('docstrap')
-  })
+    expect(guessRenderer('./node_modules/ink-docstrap/template')).toBe('docstrap');
+  });
 
   test('guess minami', () => {
-    expect(guessRenderer('node_modules/minami')).toBe('minami')
-  })
+    expect(guessRenderer('node_modules/minami')).toBe('minami');
+  });
 
   test('guess tui', () => {
-    expect(guessRenderer('node_modules/tui-jsdoc-template')).toBe('tui')
-  })
+    expect(guessRenderer('node_modules/tui-jsdoc-template')).toBe('tui');
+  });
 
   test('guess unsupported', () => {
-    console.warn = jest.fn()
+    console.warn = jest.fn();
 
-    expect(guessRenderer('foo-bar')).toBe('default')
-    expect(console.warn).toHaveBeenCalledWith(`The template "foo-bar" is not recognized by jsdoc-vuejs. Using default template as fallback.`)
-  })
-})
+    expect(guessRenderer('foo-bar')).toBe('default');
+    expect(console.warn).toHaveBeenCalledWith('The template "foo-bar" is not recognized by jsdoc-vuejs. Using default template as fallback.');
+  });
+});
--- cypress/integration/renderers/default.spec.js
+++ cypress/integration/renderers/default.spec.js
@@ -1,17 +1,17 @@
 describe('Renderers: default', () => {
   beforeEach(() => {
-    cy.visit('/../../../example/docs/BetterCounter.html')
-  })
+    cy.visit('/../../../example/docs/BetterCounter.html');
+  });
 
   it('should renders props correctly', () => {
-    cy.get('[data-vue="section-props"]').contains('Props')
-  })
+    cy.get('[data-vue="section-props"]').contains('Props');
+  });
 
   it('should renders data correctly', () => {
-    cy.get('[data-vue="section-data"]').contains('Data')
-  })
+    cy.get('[data-vue="section-data"]').contains('Data');
+  });
 
   it('should renders computed correctly', () => {
-    cy.get('[data-vue="section-computed"]').contains('Computed')
-  })
-})
+    cy.get('[data-vue="section-computed"]').contains('Computed');
+  });
+});
