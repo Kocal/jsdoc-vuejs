5c72560026e60cb688d29de881f250c13f2959dc
Hugo Alliaume <kocal@live.fr>
2018-06-21 22:42:58 +0200

    chore(tests): prepare fixtures for testing `export default` seeking

---
+++ __tests__/core/__fixtures__/methods/ComponentWithExportDefaultInTemplate.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>export default</div>
+</template>
+
+<script>
+export default {
+  name: 'Component',
+  methods: {
+    /**
+     * Show a dialog
+     */
+    showDialog() {
+      // ...
+    }
+  }
+};
+</script>
