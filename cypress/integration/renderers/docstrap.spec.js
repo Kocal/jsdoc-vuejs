4552002e542ab12beef817c4c61144a4b54915d9
Hugo Alliaume <kocal@live.fr>
2018-06-30 09:34:18 +0200

    WIP: refactor(spec): improve the writing of data/computed/props testing (#86)
    refactor(spec): improve the writing of data/computed/props testing

--- cypress/integration/renderers/docstrap.spec.js
+++ cypress/integration/renderers/docstrap.spec.js
@@ -1,112 +1,129 @@
+/* eslint-disable newline-per-chained-call */
+
 describe('Renderers: docstrap', () => {
   before(() => {
     cy.visit('/../../../example/docs-docstrap/BetterCounter.html');
   });
 
   it('should renders props correctly', () => {
+    const props = [
+      {
+        name: '<b>initialCounter</b>',
+        type: 'Number',
+        defaultValue: '-',
+        required: '<b>Yes</b>',
+        description: '-',
+      },
+      {
+        name: '<b>step</b>',
+        type: 'Number',
+        defaultValue: '<code>1</code>',
+        required: 'No',
+        description: 'Step',
+      },
+    ];
+
     cy.get('[data-jsdoc-vuejs="section-props"]').contains('Props').should('have.class', 'subsection-title');
     cy.get('[data-jsdoc-vuejs="table-props"]').as('table-props').should('have.class', 'table table-responsive table-hover table-striped');
 
     cy
       .get('@table-props')
       .find('> thead > tr > th')
-      .should(($headers) => {
-        expect($headers).to.have.length(5);
-        expect($headers.eq(0).text()).to.contains('Name');
-        expect($headers.eq(1).text()).to.contains('Type');
-        expect($headers.eq(2).text()).to.contains('Default value');
-        expect($headers.eq(3).text()).to.contains('Required ?');
-        expect($headers.eq(4).text()).to.contains('Description');
-      });
+      .contains('Name')
+      .next().contains('Type')
+      .next().contains('Default value')
+      .next().contains('Required ?')
+      .next().contains('Description');
 
     cy
       .get('@table-props')
       .find('> tbody > tr')
       .then(($rows) => {
-        const $firstRowChildren = $rows.eq(0).children();
-        const $secondRowChildren = $rows.eq(1).children();
-
         expect($rows).to.have.length(2);
 
-        expect($firstRowChildren.eq(0).html()).to.eq('<b>initialCounter</b>');
-        expect($firstRowChildren.eq(1).html()).to.eq('Number');
-        expect($firstRowChildren.eq(2).html()).to.eq('-');
-        expect($firstRowChildren.eq(3).html()).to.eq('<b>Yes</b>');
-        expect($firstRowChildren.eq(4).html()).to.eq('-');
+        props.forEach((prop, i) => {
+          const $row = $rows.eq(i);
+          const $children = $row.children();
 
-        expect($secondRowChildren.eq(0).html()).to.eq('<b>step</b>');
-        expect($secondRowChildren.eq(1).html()).to.eq('Number');
-        expect($secondRowChildren.eq(2).html()).to.eq('<code>1</code>');
-        expect($secondRowChildren.eq(3).html()).to.eq('No');
-        expect($secondRowChildren.eq(4).html()).to.eq('Step');
+          expect($children.eq(0).html()).to.eq(prop.name);
+          expect($children.eq(1).html()).to.eq(prop.type);
+          expect($children.eq(2).html()).to.eq(prop.defaultValue);
+          expect($children.eq(3).html()).to.eq(prop.required);
+          expect($children.eq(4).html()).to.eq(prop.description);
+        });
       });
   });
 
   it('should renders data correctly', () => {
+    const data = [
+      {
+        name: '<b>counter</b>',
+        type: 'Number',
+        defaultValue: '-',
+        description: "Current counter's value",
+      },
+    ];
+
     cy.get('[data-jsdoc-vuejs="section-data"]').contains('Data').should('have.class', 'subsection-title');
     cy.get('[data-jsdoc-vuejs="table-data"]').as('table-data').should('have.class', 'table table-responsive table-hover table-striped');
 
     cy
       .get('@table-data')
       .find('> thead > tr > th')
-      .should(($headers) => {
-        expect($headers).to.have.length(4);
-        expect($headers.eq(0).text()).to.contains('Name');
-        expect($headers.eq(1).text()).to.contains('Type');
-        expect($headers.eq(2).text()).to.contains('Default value');
-        expect($headers.eq(3).text()).to.contains('Description');
-      });
+      .contains('Name')
+      .next().contains('Type')
+      .next().contains('Default value')
+      .next().contains('Description');
 
     cy
       .get('@table-data')
       .find('> tbody > tr')
       .then(($rows) => {
-        const $rowChildren = $rows.eq(0).children();
-
         expect($rows).to.have.length(1);
 
-        expect($rowChildren.eq(0).html()).to.eq('<b>counter</b>');
-        expect($rowChildren.eq(1).html()).to.eq('Number');
-        expect($rowChildren.eq(2).html()).to.eq('-');
-        expect($rowChildren.eq(3).html()).to.eq('Current counter\'s value');
+        data.forEach((d, i) => {
+          const $row = $rows.eq(i);
+          const $children = $row.children();
+
+          expect($children.eq(0).html()).to.eq(d.name);
+          expect($children.eq(1).html()).to.eq(d.type);
+          expect($children.eq(2).html()).to.eq(d.defaultValue);
+          expect($children.eq(3).html()).to.eq(d.description);
+        });
       });
   });
 
   it('should renders computed correctly', () => {
+    const computeds = [
+      { name: '<b>fooList</b>', type: 'Array.&lt;String&gt;', description: 'A list of foo' },
+      { name: '<b>barList</b>', type: 'Array.&lt;String&gt;', description: 'A list of bar' },
+      { name: '<b>message</b>', type: 'String', description: 'A message' },
+    ];
+
     cy.get('[data-jsdoc-vuejs="section-computed"]').contains('Computed').should('have.class', 'subsection-title');
-    cy.get('[data-jsdoc-vuejs="table-computed"]').as('table-data').should('have.class', 'table table-responsive table-hover table-striped');
+    cy.get('[data-jsdoc-vuejs="table-computed"]').as('table-computed').should('have.class', 'table table-responsive table-hover table-striped');
 
     cy
-      .get('@table-data')
+      .get('@table-computed')
       .find('> thead > tr > th')
-      .should(($headers) => {
-        expect($headers).to.have.length(3);
-        expect($headers.eq(0).text()).to.contains('Name');
-        expect($headers.eq(1).text()).to.contains('Type');
-        expect($headers.eq(2).text()).to.contains('Description');
-      });
+      .contains('Name')
+      .next().contains('Type')
+      .next().contains('Description');
 
     cy
-      .get('@table-data')
+      .get('@table-computed')
       .find('> tbody > tr')
       .then(($rows) => {
-        const $firstRowChildren = $rows.eq(0).children();
-        const $secondRowChildren = $rows.eq(1).children();
-        const $thirdRowChildren = $rows.eq(2).children();
-
         expect($rows).to.have.length(3);
 
-        expect($firstRowChildren.eq(0).html()).to.eq('<b>fooList</b>');
-        expect($firstRowChildren.eq(1).html()).to.eq('Array.&lt;String&gt;');
-        expect($firstRowChildren.eq(2).html()).to.eq('A list of foo');
+        computeds.forEach((computed, i) => {
+          const $row = $rows.eq(i);
+          const $children = $row.children();
 
-        expect($secondRowChildren.eq(0).html()).to.eq('<b>barList</b>');
-        expect($secondRowChildren.eq(1).html()).to.eq('Array.&lt;String&gt;');
-        expect($secondRowChildren.eq(2).html()).to.eq('A list of bar');
-
-        expect($thirdRowChildren.eq(0).html()).to.eq('<b>message</b>');
-        expect($thirdRowChildren.eq(1).html()).to.eq('String');
-        expect($thirdRowChildren.eq(2).html()).to.eq('A message');
+          expect($children.eq(0).html()).to.eq(computed.name);
+          expect($children.eq(1).html()).to.eq(computed.type);
+          expect($children.eq(2).html()).to.eq(computed.description);
+        });
       });
   });
 
