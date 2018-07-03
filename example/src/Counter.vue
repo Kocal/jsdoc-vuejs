80ea58353dcefb4ed3919294967a2d05333f3d79
Hugo Alliaume <kocal@live.fr>
2018-07-03 21:31:41 +0200

    chore(example): add new component for example

---
+++ example/src/Counter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    {{ counter }}
+  </div>
+</template>
+
+<script>
+/**
+ * @vue-data {Number} [counter=0] Counter value
+ */
+export default {
+  name: 'Counter',
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  mounted() {
+    this.interval = setInterval(() => this.increment(), 1000);
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
+  methods: {
+    /**
+     * Increment the internal counter.
+     */
+    increment() {
+      this.counter += 1;
+    }
+  }
+};
+</script>
