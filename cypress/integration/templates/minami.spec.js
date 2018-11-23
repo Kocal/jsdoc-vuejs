bcd746488d42d65395e350c9de385d51a7334c15
Hugo Alliaume <kocal@live.fr>
2018-11-23 07:04:36 +0100

    test: for `@desc` tag

--- cypress/integration/templates/minami.spec.js
+++ cypress/integration/templates/minami.spec.js
@@ -16,6 +16,12 @@ describe('Template: minami', () => {
       .contains('better-components/BetterCounter');
   });
 
+  it('should renders @desc properly', () => {
+    cy
+      .get('.container-overview')
+      .contains('BetterCounter component, like Counter component but better');
+  });
+
   it('should renders props correctly', () => {
     const props = [
       {
@@ -146,19 +152,19 @@ describe('Template: minami', () => {
       .contains('decrement()')
       .next('.description')
       .next('.details')
-      .contains('a[href="better-components_BetterCounter.vue.html#line52"]', 'line 52');
+      .contains('a[href="better-components_BetterCounter.vue.html#line53"]', 'line 53');
 
     cy.get('#increment')
       .contains('increment()')
       .next('.description')
       .next('.details')
-      .contains('a[href="better-components_BetterCounter.vue.html#line45"]', 'line 45');
+      .contains('a[href="better-components_BetterCounter.vue.html#line46"]', 'line 46');
 
     cy.get('#showDialog')
       .contains('showDialog(counter)')
       .next('.description')
       .next('.details')
-      .contains('a[href="better-components_BetterCounter.vue.html#line60"]', 'line 60');
+      .contains('a[href="better-components_BetterCounter.vue.html#line61"]', 'line 61');
 
     cy.contains('created()').should('not.exist');
   });
--- cypress/integration/templates/tui.spec.js
+++ cypress/integration/templates/tui.spec.js
@@ -20,6 +20,12 @@ describe('Template: tui', () => {
       .contains('better-components/BetterCounter');
   });
 
+  it('should renders @desc properly', () => {
+    cy
+      .get('.container-overview')
+      .contains('BetterCounter component, like Counter component but better');
+  });
+
   it('should renders props correctly', () => {
     const props = [
       {
@@ -146,15 +152,15 @@ describe('Template: tui', () => {
     cy.contains('h3', 'Methods').should('have.attr', 'class', 'subsection-title');
     cy.get('#decrement')
       .contains('decrement()')
-      .contains('a[href="better-components_BetterCounter.vue.html#line52"]', 'line 52');
+      .contains('a[href="better-components_BetterCounter.vue.html#line53"]', 'line 53');
 
     cy.get('#increment')
       .contains('increment()')
-      .contains('a[href="better-components_BetterCounter.vue.html#line45"]', 'line 45');
+      .contains('a[href="better-components_BetterCounter.vue.html#line46"]', 'line 46');
 
     cy.get('#showDialog')
       .contains('showDialog(counter)')
-      .contains('a[href="better-components_BetterCounter.vue.html#line60"]', 'line 60');
+      .contains('a[href="better-components_BetterCounter.vue.html#line61"]', 'line 61');
 
     cy.contains('created()').should('not.exist');
   });
