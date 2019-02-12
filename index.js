4d83977a61ad1b8ee2bff7a308a524fe3efe1214
Enoch Riese <enoch.riese@gmail.com>
2019-02-12 14:39:00 -0600

    fixed lint issues

--- index.js
+++ index.js
@@ -36,14 +36,12 @@ exports.handlers = {
       e.doclet.name = componentName;
       e.doclet.alias = componentName;
       e.doclet.longname = `module:${componentName}`;
-    }
-    // if it's a vue module that wasn't given a name
-    else if (e.doclet.longname.includes(".module:vue")) {
+    } else if (e.doclet.longname.includes('.module:vue')) { // if it's a vue module that wasn't given a name
       // use the default name without the added gunk
-      const newName = e.doclet.longname.replace(".module:vue", "");
+      const newName = e.doclet.longname.replace('.module:vue', '');
       e.doclet.longname = `module:${newName}`;
       // fix the name if it's a module. member names aren't affected
-      if (e.doclet.kind === "module") {
+      if (e.doclet.kind === 'module') {
         e.doclet.name = newName;
       }
     }
