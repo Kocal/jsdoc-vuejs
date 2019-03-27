91c3556f87a8faec10a9e3aecb6ed8f24cebf3ff
Neal Ormsbee <neal.ormsbee@l7informatics.com>
2019-03-27 13:36:06 -0500

    Tweaked config.js to parse config passed into jsdoc-vuejs correctly.

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
-const config = require('jsdoc/env');
+const env = require('jsdoc/env');
 const getTemplatePath = require('./lib/core/getTemplatePath');
+const config = env.conf || {};
 
 config['jsdoc-vuejs'] = config['jsdoc-vuejs'] || {};
 
 // Detect JSDoc template if not specified
 if (!Object.prototype.hasOwnProperty.call(config['jsdoc-vuejs'], 'template')) {
-  config['jsdoc-vuejs'].template = getTemplatePath(config.opts.template || 'default');
+  config['jsdoc-vuejs'].template = getTemplatePath(env.opts.template || 'default');
 }
 
 module.exports = config;
